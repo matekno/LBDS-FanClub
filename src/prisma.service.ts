36e06846bf94755ce718b23f4600505fb7d58074
matekno <mati.rapoport@gmail.com>
2024-06-27 13:35:56 -0300

    configuraciones de prisma

---
+++ src/prisma.service.ts
@@ -0,0 +1,10 @@
+// Sacado de la web de NestJS
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+    async onModuleInit() {
+        await this.$connect();
+    }
+}
